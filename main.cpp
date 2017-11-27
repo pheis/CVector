fb19dbad2272a8c894f3085b58fb17eab81651c7
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2017-11-28 00:31:32 +0200

    Remove unnecessary files

--- main.cpp
+++ main.cpp
@@ -5,12 +5,34 @@
 
 using namespace std;
 
+const auto s = 25;
+
 int main() {
-  auto v = CVector<int>(25);
-  for (int i = 0; i < 25; i++) {
+  auto v = CVector<int>(s);
+  for (int i = 0; i < s; i++) {
     v.set(i, i);
   }
-  v.print_contents();
+
+  auto v2 = v.snapshot();
+
+  for (int i = 0; i < s; i++) {
+    v.set(i, i + 100);
+  }
+
+  cout << "snapshot:" << endl;
+
+  for (int i = 0; i < s; i++) {
+    auto x = v2.get(i);
+    cout << x << endl;
+  }
+
+  cout << "mutated:" << endl;
+
+  for (int i = 0; i < s; i++) {
+    auto x = v.get(i);
+    cout << x << endl;
+  }
+  // v.print_contents();
   // auto v2 = v.snapshot();
   // for (int i = 50; i < 100; i++) {
   //  v.set(i, i);
